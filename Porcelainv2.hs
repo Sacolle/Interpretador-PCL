9331c0aac3966c65936aa2ca87a611d7e4c6fece
Pedro Colle <pedro.h.b.colle@gmail.com>
2025-03-19 01:01:37 -0300

    progresso

--- Porcelainv2.hs
+++ Porcelainv2.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+
 import Data.Map
 
 type Number = Int
@@ -26,7 +28,7 @@ data Exp = Var VarName
     | Scope' Exp -- equivalente a escopo e
     | Malloc Exp 
     | Free Exp Exp 
-    | Let Exp Number
+    | Let VarName Number
     | Assign Exp Exp
     | If Exp Exp Exp
     | While Exp Exp
@@ -38,7 +40,6 @@ data Function = DeclFunc FuncName [(VarName, Number)] Exp Function | Main Exp
 
 
 --- Ambiente de nomes
-
 data EnvValues = Frame (Map VarName Loc) | Stop
 type Env = [EnvValues]
 
@@ -82,30 +83,51 @@ data PiValues = Ploc Loc | Pnum Number | Pbot | Pnull | Pstack | Pfunc deriving 
 type Pilha = [PiValues]
 
 --- Obtém o valor da pilha da pilha de valores
-pilhaGetMv :: Loc -> Pilha -> Maybe PiValues
+pilhaGetMv :: Int -> Pilha -> Maybe PiValues
 
 pilhaGetMv _ [] = Nothing
-pilhaGetMv (0, Pilha) (h : t) = Just h
-pilhaGetMv (loc, Pilha) (h : t) = pilhaGetMv (loc - 1, Pilha) t
+pilhaGetMv 0 (h : t) = Just h
+pilhaGetMv loc (h : t) = pilhaGetMv (loc - 1) t
 
 --- Obtém o valor sintático da pilha de valores
 pilhaGet :: Loc -> Pilha -> Exp
 
-pilhaGet loc pilha = maybe 
+pilhaGet (loc, Pilha) pilha = maybe 
     (error "Local inexistente na memória")
     (\value -> Value (case value of  --se pilhaGetMv retorna Just, aplica essa função
-        Ploc loc -> Loc loc
+        Ploc ploc -> Loc ploc
         Pnum number -> Number number 
         _ -> error "falha na conversão"
     ))
     (pilhaGetMv loc pilha)
 
 --- Coloca o PiValue no Loc da Pilha
-pilhaSet :: Loc -> PiValues -> Pilha -> Pilha
+pilhaSetMv :: Number -> PiValues -> Pilha -> Pilha
 
-pilhaSet (0, Pilha) value (_ : t) = value : t 
-pilhaSet (loc, Pilha) value (h : t) = h : pilhaSet (loc - 1, Pilha) value t 
-pilhaSet _ _ [] = error "Index Out of bounds na pilha"
+pilhaSetMv 0 value (_ : t) = value : t
+pilhaSetMv loc value (h : t) = h : pilhaSetMv (loc - 1) value t
+pilhaSetMv _ _ [] = error "Index Out of bounds na pilha"
+
+--- Coloca o Value no Loc da Pilha
+pilhaSet :: Loc -> Value -> Pilha -> Pilha
+--- Parece o demônio da babilônia, mas faz a sequência:
+--- Checa se o local de inserção é valido (se é bot, num ou local)
+--- se é, coloca o valor como valor de pilha
+pilhaSet (loc, Pilha) value pilha = 
+    let isValid = maybe False (\case 
+            Pnum _ -> True
+            Ploc _ -> True
+            Pbot -> True 
+            _ -> False) 
+            (pilhaGetMv loc pilha) 
+    in if isValid then 
+        pilhaSetMv loc (case value of 
+            Number number -> Pnum number
+            Loc loc -> Ploc loc
+        ) pilha
+    else
+        error "Inserção em local de memória inválido"
+    
 
 --- Remove o topo da pilha até o valor value, esse sempre será Pstack ou Pfunc
 pilhaPop :: PiValues -> Pilha -> Pilha
@@ -165,7 +187,6 @@ memSet (loc, Memoria) n mv [] = MNull : memSet (loc - 1, Memoria) n mv []
 loc :: Int -> Mem -> Loc
 loc = loc' 0 (1, Memoria)
 
-
 loc' :: Int -> Loc -> Int -> Mem -> Loc
 loc' count (l, Memoria) size m = case memGetMv (l + count, Memoria) m of
     Nothing -> (l, Memoria) -- chegou no fim da memória
@@ -173,3 +194,75 @@ loc' count (l, Memoria) size m = case memGetMv (l + count, Memoria) m of
     Just MNull -> if size == count then
         (l, Memoria) else loc' (count + 1) (l, Memoria) size m
     Just _ -> loc' 0 (l + 1, Memoria) size m
+
+
+--- Semântica operacional
+
+-- Em haskell, declarações nesse padrão devem ser declaradas
+--  do mais específicos ao menos específico.
+
+-- Passa por todas as funções e salva ela em Funcs. Para quando chega na Main
+functionStep :: (Function, Funcs, Env, Pilha, Mem) -> (Function, Funcs, Env, Pilha, Mem)
+
+functionStep (DeclFunc funcname args body function, funcs, env, pilha, mem) =
+    functionStep (function, funcsSet funcname (args, body) funcs, env, pilha, mem)
+functionStep (Main exp, funcs, env, pilha, mem) = (Main exp, funcs, env, pilha, mem)
+
+
+expStep :: (Exp, Funcs, Env, Pilha, Mem) -> (Exp, Funcs, Env, Pilha, Mem)
+
+--- ====================
+--- Regras de composição
+--- ====================
+
+--- Compose-combine
+expStep (Comp (Value _) exp, funcs, env, pilha, mem) = 
+    expStep (exp, funcs, env, pilha, mem)
+
+--- Compose-step
+expStep (Comp exp1 exp2, funcs, env, pilha, mem) = 
+    let (exp1', _, env', pilha', mem') = expStep (exp1, funcs, env, pilha, mem) in
+        expStep (Comp exp1' exp2, funcs, env', pilha', mem')
+
+--- Escopo-init
+--- Empilha os elementos nas pilha e muda o código para Scope' 
+--- para isso ser feito só no primeiro passo
+expStep (Scope exp, funcs, env, pilha, mem) = 
+    expStep (Scope' exp, funcs, Frame empty : env, Pstack : pilha, mem)
+
+--- Escopo-pop
+--- remove-se o frame do topo do ambiente de nomes e 
+--- remove elementos da pilha até o código de controle Pop
+expStep (Scope' (Value v), funcs, env, pilha, mem) = 
+    let (_ : env') = env in -- desempilha o topo de env
+    let pilha' = pilhaPop Pstack pilha in  --- remove valores até Pstack
+    expStep (Value v, funcs, env', pilha', mem)
+
+--- Escopo-step
+expStep (Scope' exp, funcs, env, pilha, mem) = 
+    let (exp', _, env', pilha', mem') = expStep (exp, funcs, env, pilha, mem) in
+        expStep (Scope' exp', funcs, env', pilha', mem')
+
+
+--- ====================
+--- Regras de atribuição
+--- ====================
+
+--- Let
+expStep (Let nome size, funcs, env, pilha, mem) = 
+    let topoPilha = length pilha in
+    let env' = envSet nome (topoPilha, Pilha) env in -- coloca nome -> local no env de nomes
+    let pilha' = replicate size Pbot ++ pilha in -- empilha size Pbot na pilha (tamanho da variável)
+    expStep (Value (Loc (topoPilha, Pilha)), funcs, env', pilha', mem)
+
+--- Atribui-deref-pilha
+--- *lp := v
+expStep (Assign (Deref (Value (Loc (local, Pilha)))) (Value v), funcs, env, pilha, mem) = 
+    let pilha' = pilhaSet (local, Pilha) v pilha in 
+    expStep (Value v, funcs, env, pilha', mem)
+
+
+
+-- Para a avaliação quando chega em valor
+expStep (Value value, funcs, env, pilha, mem) = (Value value, funcs, env, pilha, mem)
+
