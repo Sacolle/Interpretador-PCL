2bf02390eae62fe988ea9afd8cc0131d8e27cf8a
Sacolle <pedro.h.b.colle@gmail.com>
2025-05-21 21:37:25 -0300

    não tem nullptr para endereços da pilha

--- app/Pilha.hs
+++ app/Pilha.hs
@@ -27,7 +27,6 @@ get :: Pcl.Loc -> Pilha -> Either ErrorKinds Value
 
 get loc pilha 
     | local loc == Memoria = error "chamada da função pilhaGet com um endereço de memória"
-    | idx loc == 0 = Left NullPtrDereference
     | not $ isInBounds loc = Left OutOfBoundsRead 
     | lock /= key loc = Left UseAfterFree
     | otherwise = case value of 
@@ -68,7 +67,6 @@ set :: Loc -> Value -> Pilha -> Either ErrorKinds Pilha
 --- se é, coloca o valor como valor de pilha
 set loc value pilha 
     | local loc == Memoria = error "chamada da função pilhaSet com um endereço de memória"
-    | idx loc == 0 = Left NullPtrDereference
     | not $ isInBounds loc = Left OutOfBoundsWrite
     | key loc /= lock = Left UseAfterFree
     | otherwise = maybe 
